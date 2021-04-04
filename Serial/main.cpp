6d612d6902d65d67ddb1a30a07173616eb7288b4
Steele Muchemore-Allen <steelemallen@gmail.com>
2021-04-04 16:26:59 -0400

    Added cornell box as hittable object. Output all black currently.

--- Serial/main.cpp
+++ Serial/main.cpp
@@ -117,16 +117,36 @@ Hittable_List two_fuzzy_balls()
     return world;
 }
 
-Hittable_List simple_light() {
+Hittable_List simple_light() 
+{
     Hittable_List objects;
 
     auto pertext = make_shared<Noise_Text>(4);
     objects.add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, make_shared<Lambertian>(pertext)));
     objects.add(make_shared<Sphere>(Point3(0, 2, 0), 2, make_shared<Lambertian>(pertext)));
 
-    auto difflight = make_shared<diffuse_light>(Color(4, 4, 4));
+    auto difflight = make_shared<Diffuse_Light>(Color(4, 4, 4));
     objects.add(make_shared<Sphere>(Point3(0, 7, 0), 2, difflight));
     objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
+
+    return objects;
+}
+
+Hittable_List cornell_box()
+{
+    Hittable_List objects;
+
+    auto red   = make_shared<Lambertian>(Color(.65, .05, .05));
+    auto white = make_shared<Lambertian>(Color(.73, .73, .73));
+    auto green = make_shared<Lambertian>(Color(.12, .45, .15));
+    auto light = make_shared<Diffuse_Light>(Color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
     return objects;
 }
@@ -172,11 +192,11 @@ int main(int argc, char **argv)
     }
     
     // Image
-    const float aspect_ratio = 16.0f / 9.0f;
+    float aspect_ratio = 16.0f / 9.0f;
     int image_width = 400;
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    int samples_per_pixel = 500;
-    int max_depth = 100;
+    int samples_per_pixel = 100;
+    int max_depth = 50;
+    int image_height;
 
     // World
     Hittable_List world;
@@ -241,7 +261,7 @@ int main(int argc, char **argv)
             aperture = 0;
             break;
 
-        default:
+
         case 5:
             world = simple_light();
             lookfrom = Point3(26, 3, 6);
@@ -252,10 +272,24 @@ int main(int argc, char **argv)
             dist_to_focus = 10.0;
             aperture = 0;
             break;
+
+        default:
+        case 6:
+            world = cornell_box();
+            aspect_ratio = 1.0;
+            image_width = 200;
+            background = Color(0, 0, 0);
+            lookfrom = Point3(278, 278, -800);
+            lookat = Point3(278, 278, 0);
+            fov = 40;
+           dist_to_focus = 10.0;
+            aperture = 0;
+            break;
     }
 
     // Camera
     Camera cam(lookfrom, lookat, vup, fov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Output File
     std::fstream file;
