9c3dabfb732d4e15d5d1c67f0417e6070a4c4295
Steele Muchemore-Allen <steelemallen@gmail.com>
2021-04-04 16:26:14 -0400

    Added remaining two axis rectangles.

--- Serial/Aarect.h
+++ Serial/Aarect.h
@@ -26,4 +26,37 @@ private:
     shared_ptr<Material> mp;
 };
 
+
+class xz_rect : public Hittable
+{
+public:
+     xz_rect() {}
+
+    xz_rect(float _x0, float _x1, float _z0, float _z1, float _k, shared_ptr<Material> mat)
+        : x0(_x0), x1(_x1), z0(_z0), z1(_z1), k(_k), mp(mat) {};
+
+    virtual bool hit(const Ray& r, float t_min, float t_max, hit_record& rec) const override;
+    virtual bool bounding_box(float time0, float time1, Aabb& output_box) const override;
+        
+private:
+    float x0, x1, z0, z1, k;
+    shared_ptr<Material> mp;
+};
+
+class yz_rect : public Hittable
+{
+public:
+     yz_rect() {}
+
+    yz_rect(float _y0, float _y1, float _z0, float _z1, float _k, shared_ptr<Material> mat)
+        : y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(mat) {};
+
+    virtual bool hit(const Ray& r, float t_min, float t_max, hit_record& rec) const override;
+    virtual bool bounding_box(float time0, float time1, Aabb& output_box) const override;
+        
+private:
+    float y0, y1, z0, z1, k;
+    shared_ptr<Material> mp;
+};
+
 #endif
