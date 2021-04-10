bb7529dc92e10680f4f4e9aaa9c563d33659604d
steele Muchemore-Allen <steele.muchemoreallen@maine.edu>
2021-04-10 11:56:31 -0400

    Added aabb to CUDA.

---
+++ Cuda/Aabb.h
@@ -0,0 +1,35 @@
+/*
+ * Axis Aligned Box Class header file
+ */
+
+#ifndef AABB_H
+#define AABB_H
+
+#include "Vec3.h"
+#include "Ray.h"
+#include "shader_consts.h"
+
+class Aabb
+{
+public:
+    // Constructors
+    __device__ Aabb();
+    __device__ Aabb(const Point3 &a, const Point3 &b);
+
+    // Getters
+    __device__ Point3 max() const;
+    __device__ Point3 min() const;
+
+    __device__ bool hit(const Ray &r, float t_min, float t_max) const;
+
+private:
+
+    Point3 maximum;
+    Point3 minimum;
+
+};
+
+// Utility Function
+__device__ Aabb surrounding_box(Aabb box0, Aabb box1);
+
+#endif //AABB_H
