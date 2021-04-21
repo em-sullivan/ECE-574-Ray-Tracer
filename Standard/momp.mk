ab169dbda679c2011a7907e9ba6db60f818048de
Steele Muchemore-Allen <steelemallen@gmail.com>
2021-04-21 00:23:40 -0400

    Added include path, not sure if needed in both targets?

--- Standard/momp.mk
+++ Standard/momp.mk
@@ -2,18 +2,19 @@ CC = g++
 CFLAGS = -Wall -o2 -g -fopenmp
 LDFLAGS = -fopenmp
 OBJS = Color.o Vec3.o Ray.o Sphere.o Hittable_List.o Camera.o Material.o Moving_Sphere.o Bvh_Node.o Aabb.o Texture.o Perlin.o Aarect.o Box.o Translate.o Constant_Medium.o render_omp.o worlds.o
+INC = -I../Common/include
 
 N = 8
 
 
 rayer-tracer: main.o $(OBJS)
-	$(CC) $(LDFLAGS) -o rayer-tracer.elf main.o $(OBJS)
+	$(CC) $(LDFLAGS) -o rayer-tracer.elf main.o $(OBJS) 
 
 main.o: main.cpp
-	$(CC) $(CFLAGS) -o main.o -c main.cpp
+	$(CC) $(CFLAGS) $(INC) -o main.o -c main.cpp
 
 %.o : %.cpp
-	$(CC) $(CFLAGS) -o $@ -c $<
+	$(CC) $(CFLAGS) $(INC) -o $@ -c $<
 
 create-image:
 	./rayer-tracer.elf $(N) $(W) $(H) $(S)
